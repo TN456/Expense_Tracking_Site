46f80460de12b9af982fafa855114a8b57cf8037
Tanuja <68894643+TN456@users.noreply.github.com>
2022-11-28 08:22:48 +0530

    Add files via upload

---
+++ Expense Tracker Frontend/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Base from '../Components/Base';
+
+const About = () => {
+  return (
+    <Base>
+      <h1>This is ABOUT page</h1>
+    </Base>
+  )
+}
+
+export default About;
